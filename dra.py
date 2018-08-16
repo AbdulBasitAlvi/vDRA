e32b2f51392e46cc01afa18d857bedc94e416544
Abdul Basit Alvi <abdulbasit.ta@gmail.com>
2018-08-16 16:36:16 +0500

    Polling and Queues
    Added Polling to PCRF and a Queue to handle messages efficiently

--- dra.py
+++ dra.py
@@ -2,11 +2,17 @@ import socket
 import binascii
 from libDiameter import *
 from msgDiameter import *
+import datetime
+import select
+import sys
+import Queue
+
 
 PCRF_PORT = 5001
 PCRF_IP = "10.248.125.144"
 DRA_PORT = 6000
 DRA_IP = "10.150.32.2"
+ERROR = -2
 SKIP = 2 # Flag used in Skip message for message processing
 IS_CCR = 0 # Flag for CCR, If 1 then message is a CCR request
 IS_CCA = 1 # Flag for CCA
@@ -20,9 +26,6 @@ def process_request(rawdata):
     stripHdr(H,rawdata)
     dbg="Processing",dictCOMMANDcode2name(H.flags,H.cmd)
     logging.info(dbg)
-    if H.flags & DIAMETER_HDR_REQUEST==0:
-        # If Answer no need to do anything  
-        return SKIP
     if H.cmd==257: # Capabilities-Exchange  
         return create_CEA(rawdata)
     if H.cmd==280: # Device-Watchdog  
@@ -31,88 +34,128 @@ def process_request(rawdata):
         variable=splitMsgAVPs(H.msg)
         if H.flags==192:
             DEST_HOST=findAVP("Destination-Host",variable)
-            print ("This is a Credit Control Request for Destination Host : " + DEST_HOST)
+            print ("This is a Credit Control Request for Destination Host : " + str(DEST_HOST))
             return IS_CCR
         else:
-            DEST_HOST=findAVP("Destination-Host",variable)
-            print ("This is a Credit Control Answer for Destination Host : " + DEST_HOST)
+            #DEST_HOST=findAVP("Destination-Host",variable)
             return IS_CCA
-
-    return create_UTC(rawdata,"Unknown command code")         
+    else:
+        return ERROR
 
 
 def server_program():
 
     pcrf_socket = socket.socket()  # Instantiate PCRF connection
+    #pcrf_socket.settimeout(10) #Set socket timeout to 5 seconds
     try:
         pcrf_socket.connect((PCRF_IP, PCRF_PORT))  # Connect to PCRF
         print ("Connected to PCRF")
         pcrf_socket.send(create_CER().decode('hex'))  # Send CER to PCRF
         print ("Sent Capabilities-Exchange Request")
-        print("Recieved CER")
         try:
-            pcrf_data = pcrf_socket.recv(10000).encode('hex')  # Receive CEA from PCRF
-            DecodeMSG(pcrf_data)
+            pcrf_data = pcrf_socket.recv(30000).encode('hex')  # Receive CEA from PCRF
+            print("Recieved CEA")
+            #DecodeMSG(pcrf_data)
         except socket.error:
             print ("Not connected to PCRF")
     except socket.error:
         print ("Could not connect to PCRF")
 
 
-    ggsn_socket = socket.socket() 
+
+    ggsn_socket = socket.socket()
+    ggsn_socket.setblocking(0) 
+    # ggsn_socket.settimeout(5) #Set socket timeout to 1 second
     ggsn_socket.bind((DRA_IP, DRA_PORT))  # Instantiate GGSN connection
     ggsn_socket.listen(2)  #Configure how many client the server can listen simultaneously
-    conn, address = ggsn_socket.accept()  # Accept new connection
+    sockets = [pcrf_socket , ggsn_socket]
+    outputs = [ ]
+    message_queues = {}
 
-    print("Connection from: " + str(address))
+    # Give the PCRF connection a queue for data we want to send
+    message_queues[pcrf_socket] = Queue.Queue()
 
-    while True:
-        # Recieve data from PCRF
-        pcrf_data = pcrf_socket.recv(10000).encode('hex')
-        if not pcrf_data:
-        # If data is not recieved from PCRF
-            print ("No data to recieve from PCRF")
-        else:
-            pcrf_return_value = process_request(pcrf_data)
-            print ("Incomming Data from PCRF")
-            DecodeMSG(pcrf_data)       
-            if pcrf_return_value == ERROR:
-                print ("Error decoding Diameter message")
-            if pcrf_return_value == IS_CCR:
-                conn.send(pcrf_data.decode('hex')) # Send CCA to GGSN
-            else:
-                if pcrf_return_value == SKIP:
-                    print ("This is an Answer message")
+
+    loop_iteration = 1
+
+
+    while True:      
+        print(str(datetime.datetime.now()) + " Loop Iteration = " + str(loop_iteration))
+
+        # Use select to poll through sockets
+        read,write,error = select.select(sockets,outputs,[])
+        print read
+        for rs in read: # Iterate through readable sockets
+            print(str(datetime.datetime.now()) + " Readable Socket!")
+            if rs is pcrf_socket: # It is PCRF socket
+                print(str(datetime.datetime.now()) + " Readable PCRF Socket!")
+                pcrf_data = pcrf_socket.recv(30000).encode('hex')
+                if not pcrf_data:
+                    print(str(datetime.datetime.now()) + ' Disconnected from PCRF')
+                    sockets.remove(rs)
+                    rs.close()
                 else:
-                    print ("Sending reply to PCRF")
-                    print (DecodeMSG(pcrf_return_value.upper()))
-                    pcrf_socket.send(pcrf_return_value.upper().decode('hex')) #Send CEA and DWA to PCRF
+                    print (str(datetime.datetime.now()) + " Incomming Data from PCRF")
+                    pcrf_return_value = process_request(pcrf_data) #Process Data from PCRF
+                    if pcrf_return_value == IS_CCA:
+                        print (str(datetime.datetime.now()) +" Sending CCA to GGSN")
+                        conn.send(pcrf_data.decode('hex'))
+                        print (str(datetime.datetime.now()) +" CCA sent to GGSN") 
+                        conn.send(pcrf_data)            
+                    else:
+                        print (str(datetime.datetime.now()) +" Saving CEA/DWA message to PCRF queue")
+                        message_queues[rs].put(pcrf_return_value) #Add message to PCRF Queue
+                        if rs not in outputs:   #Add to writable sockets 
+                            outputs.append(rs)
+                            
+            if rs is ggsn_socket:
+                conn, address = ggsn_socket.accept()  # Accept new connection on GGSN Socket
+                print("Connection to GGSN on IP address: " + str(address))
+                print(str(datetime.datetime.now()) + " Readable GGSN Socket!")
+                print (str(datetime.datetime.now()) + " Fetching Data from GGSN")
+                sockets.append(conn)
+            else: 
+                ggsn_data = conn.recv(30000).encode('hex') # GGSN has data to send!
+                if not ggsn_data:
+                    print(str(datetime.datetime.now()) + ' Disconnected from GGSN')
+                    sockets.remove(rs)
+                    rs.close()
+                else:
+                    ggsn_return_value = process_request(ggsn_data)  
+                    if ggsn_return_value == ERROR:
+                        print (str(datetime.datetime.now()) + " Error decoding Diameter message")
+                    if ggsn_return_value == IS_CCR:
+                        print (str(datetime.datetime.now()) +" Saving CCR message to PCRF queue")
+                        message_queues[pcrf_socket].put(ggsn_data) #Add message to PCRF Queue
+                        if rs not in outputs:   #Add to writable sockets 
+                            outputs.append(rs)
+                    else:
+                        if ggsn_return_value == SKIP:
+                            print (str(datetime.datetime.now()) + " This is an Answer message")
+                        else:
+                            print (str(datetime.datetime.now()) + " Sending CEA/DWA to GGSN")
+                            conn.send(ggsn_return_value.upper().decode('hex')) #Send CEA and DWA to GGSN
 
 
-        # Recieve data from GGSN
-        ggsn_data = conn.recv(10000).encode('hex') 
-        if not ggsn_data:
-        # If data is not received from GGSN skip the loop
-            continue
-
-        #Decode diameter Header from GGSN
-        diameter_header(ggsn_data)
-        DecodeMSG(ggsn_data)
-
-        # Process incomming data from GGSN
-        ggsn_return_value = process_request(ggsn_data)        
-        if ggsn_return_value == ERROR:
-            print ("Error decoding Diameter message")
-        if ggsn_return_value == IS_CCR:
-            pcrf_socket.send(ggsn_data.decode('hex')) # Send CCR to PCRF
-        else:
-            if ggsn_return_value == SKIP:
-                print ("This is an Answer message")
+        for s in write:
+            print(str(datetime.datetime.now()) + "Writable PCRF Socket!")
+            try:
+                next_msg = message_queues[s].get_nowait()
+            except Queue.Empty:
+                # No messages waiting so stop checking for writability.
+                print (str(datetime.datetime.now()) + " PCRF Queue is Empty!")
+                outputs.remove(s)
+            except KeyError:
+                print (str(datetime.datetime.now()) + " KeyError!")
             else:
-                conn.send(ggsn_return_value.upper().decode('hex')) #Send CEA and DWA to GGSN
+                s.send(next_msg.upper().decode('hex')) #Send CEA and DWA to PCRF
+                if ggsn_return_value == IS_CCR:
+                    print(str(datetime.datetime.now()) + " CCR sent to PCRF")
+                if pcrf_return_value is not  == SKIP:
+                    print(str(datetime.datetime.now()) + " CEA/DWA sent to PCRF")
 
-    conn.close()  # close the connection
-    pcrf_socket.close()
+
+        loop_iteration = loop_iteration + 1
 
 
 if __name__ == '__main__':
